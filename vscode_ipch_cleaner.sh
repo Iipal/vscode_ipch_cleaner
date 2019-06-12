9e8ce4e39820d56a8a43dc164a56307b54e02a60
Iipal <malkoleyplay@gmail.com>
2019-06-12 12:09:43 +0300

    Update vscode_ipch_cleaner.sh

--- vscode_ipch_cleaner.sh
+++ vscode_ipch_cleaner.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
+if [[ -z "$1" ]]; then
 	find ~ -name "ipch" -exec rm -rfv {} \;
 	exit
 fi
 
 if [ "$1" == "-f" ]; then
-	find ~ -name "ipch"
-elif [ "$1" == "-p" ]; then
-	if [ "$2" != "" ]; then
-		find "$2" -name "ipch" -exec rm -rfv {} \;
+	if [ -z "$2" ]; then
+		echo "Missing argument for -f. Search starts from ~."
+		find ~ -name "ipch"
 	else
-		echo "Missing argument for -p."
+		find "$2" -name "ipch"
+	fi
+elif [ "$1" == "-p" ]; then
+	if [ -z "$2"]; then
+		echo -n "Missing argument for -p. Removing will starts from ~. (yes or no) -> "
+		read is_remove
+		if [ "$is_remove" == "yes" ]; then
+			find ~ -name "ipch" -exec rm -rfv {} \;
+		else
+			echo "Cancellation deleting from ~."
+		fi
+	else
+		find "$2" -name "ipch" -exec rm -rfv {} \;
 	fi
 else
-	echo "Incorrect argument." 
+	echo "Incorrect argument."
 fi
 exit
