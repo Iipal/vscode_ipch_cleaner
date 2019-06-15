02d8d9bf22f56eb0cb864bae98a38b09962a79c5
Iipal <malkoleyplay@gmail.com>
2019-06-15 14:36:30 +0300

    Update vscode_ipch_cleaner.sh

--- vscode_ipch_cleaner.sh
+++ vscode_ipch_cleaner.sh
@@ -2,8 +2,19 @@
 
 if [ -z "$1" ]; then
 	find ~ -name "ipch" -exec rm -rfv {} \;
+	echo -e "\033[32mDone.\033[0m"
 	exit
 fi
+
+default_remove_after_ask() {
+	echo -n " (yes or no) -> "
+	read is_remove
+	if [ "$is_remove" == "yes" ]; then
+		find ~ -name "ipch" -exec rm -rfv {} \;
+	else
+		echo "Cancellation deleting from ~."
+	fi
+}
 
 if [ "$1" == "-f" ]; then
 	if [ -z "$2" ]; then
@@ -12,18 +23,27 @@ if [ "$1" == "-f" ]; then
 	else
 		find "$2" -name "ipch"
 	fi
+	echo -e "\033[32mDone.\033[0m"
 elif [ "$1" == "-p" ]; then
 	if [ -z "$2"]; then
-		echo -n "Missing argument for -p. Removing will starts from ~. (yes or no) -> "
-		read is_remove
-		if [ "$is_remove" == "yes" ]; then
-			find ~ -name "ipch" -exec rm -rfv {} \;
-		else
-			echo "Cancellation deleting from ~."
-		fi
+		echo -n "Missing argument for -p. Removing will starts from ~."
+		default_remove_after_ask
 	else
 		find "$2" -name "ipch" -exec rm -rfv {} \;
+		echo -e "\033[32mDone.\033[0m"
+	fi
+elif [ "$1" == "-q" ]; then
+	if [ -z "$2" ]; then
+		echo -n "Missing deleting directory path for -q(1 argument). Removing will starts not quiet from ~."
+		default_remove_after_ask
+	elif [ -z "$3" ]; then
+		echo -n "Missing quiet mode output path to file for -q(2 argument). Removing will starts not quiet from ~."
+		default_remove_after_ask
+	else
+		find "$2" -name "ipch" -exec rm -rfv {} \; >> "$3"
+		echo -e "\033[32mDone.\033[0m"
 	fi
 else
 	echo "Incorrect argument."
+	exit
 fi
