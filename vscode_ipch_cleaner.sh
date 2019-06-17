a9ff9ab2e820bfa0437994a6ec7ad7e5aa8851af
Iipal <malkoleyplay@gmail.com>
2019-06-17 11:07:04 +0300

    Update vscode_ipch_clenaer.sh

--- vscode_ipch_cleaner.sh
+++ vscode_ipch_cleaner.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+BEFORE="$(df -h | grep "$(whoami)" | awk {'print $4'})"
+
 if [ -z "$1" ]; then
+	echo -e $'\e[38;5;196mFree space Before:' $BEFORE $'\e[0m'
 	find ~ -name "ipch" -exec rm -rfv {} \;
-	echo -e "\033[32mDone.\033[0m"
+	AFTER="$(df -h | grep "$(whoami)" | awk {'print $4'})"
+	echo -e $'\e[38;5;46mFree space After:' $AFTER $'\e[0m'
+	echo -e $'\e[38;5;46mDone.\e[0m'
 	exit
 fi
 
@@ -10,7 +15,10 @@ default_remove_after_ask() {
 	echo -n " (yes or no) -> "
 	read is_remove
 	if [ "$is_remove" == "yes" ]; then
+		echo -e $'\e[38;5;196mFree space Before:' $BEFORE $'\e[0m'
 		find ~ -name "ipch" -exec rm -rfv {} \;
+		AFTER="$(df -h | grep "$(whoami)" | awk {'print $4'})"
+		echo -e $'\e[38;5;46mFree space After:' $AFTER $'\e[0m'
 	else
 		echo "Cancellation deleting from ~."
 	fi
@@ -25,11 +33,14 @@ if [ "$1" == "-f" ]; then
 	fi
 	echo -e "\033[32mDone.\033[0m"
 elif [ "$1" == "-p" ]; then
-	if [ -z "$2"]; then
+	if [ -z "$2" ]; then
 		echo -n "Missing argument for -p. Removing will starts from ~."
 		default_remove_after_ask
 	else
+		echo -e $'\e[38;5;196mFree space Before:' $BEFORE $'\e[0m'
 		find "$2" -name "ipch" -exec rm -rfv {} \;
+		AFTER="$(df -h | grep "$(whoami)" | awk {'print $4'})"
+		echo -e $'\e[38;5;46mFree space After:' $AFTER $'\e[0m'
 		echo -e "\033[32mDone.\033[0m"
 	fi
 elif [ "$1" == "-q" ]; then
